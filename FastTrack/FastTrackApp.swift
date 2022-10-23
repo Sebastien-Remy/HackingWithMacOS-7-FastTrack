486364b838807940d705e4b343faffbf7047afd4
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-23 17:05:10 +0200

    Initial Commit

---
+++ FastTrack/FastTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTrackApp.swift
+//  FastTrack
+//
+//  Created by Sebastien REMY on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FastTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
