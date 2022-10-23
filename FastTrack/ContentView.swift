cd8ad55e8ff1f76f7ad94bc1fdc74dc10c568966
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-23 18:50:56 +0200

    Downloading data from server

--- FastTrack/ContentView.swift
+++ FastTrack/ContentView.swift
@@ -17,23 +17,38 @@ struct ContentView: View {
     
     @AppStorage("searchText") var searchText = ""
     
+    @State private var tracks = [Track]()
+    
     var body: some View {
         VStack {
             HStack {
                 TextField("Search for a song", text: $searchText)
-                Button("Search") {
-                }
+                    .onSubmit(startSearch)
+                Button("Search", action: startSearch)
             }
             
             ScrollView {
                 LazyVGrid(columns: gridItems) {
-                    ForEach(1..<100) {
-                        i in
-                        Color.red
+                    ForEach(tracks) { track in
+                        Text(track.trackName)
                             .frame(width: 150, height: 150)
                     }
                 }
             }
+        }
+    }
+    
+    func performSearch() async throws {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=\(searchText)&limit=100&entity=song") else { return }
+        let (data, _) =  try await URLSession.shared.data(from: url)
+        let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+        tracks = searchResult.results
+        
+    }
+    
+    func startSearch() {
+        Task {
+            try await performSearch()
         }
     }
 }
